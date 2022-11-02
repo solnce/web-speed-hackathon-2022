94e6919e4fb3a41fd816373324bb5bd5786768a8
solnce <solnce.jpn@gmail.com>
2022-11-03 03:00:48 +0900

    oops

--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,7 @@ module.exports = [
       new CopyPlugin({
         patterns: [{ from: PUBLIC_ROOT, to: DIST_PUBLIC }],
       }),
-      process.env.NODE_ENV === "production" && new BundleAnalyzerPlugin(),
+      // process.env.NODE_ENV === "production" && new BundleAnalyzerPlugin(),
     ],
     resolve: {
       extensions: [".js", ".jsx"],
