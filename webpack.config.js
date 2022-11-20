96e2185dcf71c8b22f24dc61af828ef81ea5ebfc
solnce <solnce.jpn@gmail.com>
2022-11-20 21:36:38 +0900

    refactor

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,6 @@ const DIST_PUBLIC = abs("./dist/public");
 /** @type {Array<import('webpack').Configuration>} */
 module.exports = [
   {
-    devtool: "source-map",
     entry: path.join(SRC_ROOT, "client/index.jsx"),
     mode: "production",
     module: {
@@ -80,7 +79,6 @@ module.exports = [
     target: "web",
   },
   {
-    devtool: "source-map",
     entry: path.join(SRC_ROOT, "server/index.js"),
     externals: [nodeExternals()],
     mode: "development",
